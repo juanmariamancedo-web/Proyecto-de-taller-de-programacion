248b3178c24f87df2888c6900cba2d28f9e3455d
Juan Mancedo <juanmaninc@icloud.com>
2026-05-22 14:42:00 -0300

    ahora poseemos toast para notificar que se agrego un producto al carrito

--- resources/js/Pages/Producto.tsx
+++ resources/js/Pages/Producto.tsx
@@ -6,8 +6,9 @@ import { useState } from "react";
 import { router, usePage } from "@inertiajs/react";
 
 export default function Producto({producto} : {producto : Producto}){
-    const dispatch = useDispatch();
+    const [showToast, setShowToast] = useState(false);
     const [cantidad, setCantidad] = useState(1);
+    const dispatch = useDispatch();
     const props = usePage().props as any;
 
     function addProducto(){
@@ -20,30 +21,40 @@ export default function Producto({producto} : {producto : Producto}){
             cantidad: cantidad
             },
         ));
+
+        setShowToast(true);
+
+        setTimeout(() => setShowToast(false), 2000);
     }
 
     return(
         <MainLayout>
-            <div className="p-3 sm:p-0 grid grid-cols-2 gap-3 auto-rows-[384px]">
-                <div className="flex items-center justify-center col-span-2 sm:col-span-1">
-                    <img src={producto.image} alt={"Imagen de " + producto.name} className="max-w-full max-h-full object-contain rounded-md" />
-                </div>
-                <div className="flex flex-col items-center justify-evenly col-span-2 sm:col-span-1">
-                    <h1 className="text-center text-2xl/9 font-bold tracking-tight text-gray-900 dark:text-white">{producto.name}</h1>
-                    <div>
-                        <p className="text-base font-bold">
-                            ${producto.price.toLocaleString('es-AR')}
-                        </p>
-                        <p className="text-base font-medium">
-                            {producto.description}
-                        </p>
+            <>
+                
+                <div className="p-3 sm:p-0 grid grid-cols-2 gap-3 auto-rows-[384px]">
+                    <div className="flex items-center justify-center col-span-2 sm:col-span-1">
+                        <img src={producto.image} alt={"Imagen de " + producto.name} className="max-w-full max-h-full object-contain rounded-md" />
                     </div>
-                    <input type="number" value={cantidad} onChange={(e)=>setCantidad(parseInt(e.target.value))}  className="rounded-md bg-black/5 px-3 py-1.5 text-gray-900 outline-1 outline-gray-300 focus:outline-2 focus:outline-indigo-600 dark:bg-white/5 dark:text-white"/>
-                    <button className=" bg-indigo-600 text-white p-2 rounded-md disabled:opacity-50" onClick={addProducto}>
-                        Añadir al carrito
-                    </button>
+                    <div className="flex flex-col items-center justify-evenly col-span-2 sm:col-span-1">
+                        <h1 className="text-center text-2xl/9 font-bold tracking-tight text-gray-900 dark:text-white">{producto.name}</h1>
+                        <div>
+                            <p className="text-base font-bold">
+                                ${producto.price.toLocaleString('es-AR')}
+                            </p>
+                            <p className="text-base font-medium">
+                                {producto.description}
+                            </p>
+                        </div>
+                        <input type="number" value={cantidad} onChange={(e)=>setCantidad(parseInt(e.target.value))}  className="rounded-md bg-black/5 px-3 py-1.5 text-gray-900 outline-1 outline-gray-300 focus:outline-2 focus:outline-indigo-600 dark:bg-white/5 dark:text-white"/>
+                        <button className=" bg-indigo-600 text-white p-2 rounded-md disabled:opacity-50" onClick={addProducto}>
+                            Añadir al carrito
+                        </button>
+                        <span className={` ${showToast? "": "invisible "}bg-black/5 px-3 py-1.5 text-base text-gray-900 sm:text-sm/6 dark:bg-white/5 dark:text-white rounded`}>
+                            Producto agregado
+                        </span>
+                    </div>
                 </div>
-            </div>
+            </>
         </MainLayout>
     )
 }
--- resources/js/layouts/MainLayout.tsx
+++ resources/js/layouts/MainLayout.tsx
@@ -38,7 +38,7 @@ export default function MainLayout({children}:{children: JSX.Element}){
                     dark:bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"
                 ></div>
                 <Header pages={clientPages} homeUrl="/"/>
-                <main className="container mx-auto pt-14">
+                <main className="container mx-auto pt-14 relative">
                     {children}
                 </main>
                 <Carrito />
